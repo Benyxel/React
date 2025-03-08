b6c3a097ec3df5ba06f26ee60b1b67c5b153ab09
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-08 22:37:04 +0000

    new

---
+++ first project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const App = () => {
+
+ const [x,setx] = useState(0);
+
+ const btnClick = () => {
+  console.log("clicked");
+  setx(x+1)
+  console.log(x);
+ }
+  return(
+   <div>
+      {x}
+     <button onClick={()=>{btnClick()}}>Click me</button>
+    </div>
+  )
+}
+
+export default App
