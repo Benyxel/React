7c659ae508dda7ff14951c1e0de28c5dea538513
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-10 22:35:50 +0000

    new

--- project-demo/src/App.jsx
+++ project-demo/src/App.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import {createRoot} from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 root.render(
-  <main>
-     <img src="/src/assets/react.svg" width={100}/>
-    <h1>Hello are you there</h1>
+  <>
+    <img src="/src/assets/react.svg" width={100}/>
+    <header><h1>Hello this is the header</h1></header>
     <ul>
       <li>list number one</li>
       <li>list number two</li>
       <li>list number three</li>
       <li>list number four</li>
     </ul>
-  </main>
+    <footer><span>my footer is this one </span></footer>
+  </>
    
     
 );
