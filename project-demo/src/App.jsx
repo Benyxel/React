c0d2bd4e4b35bbfd245eecc4c7fad2a82afbb987
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-10 22:47:46 +0000

    new

--- project-demo/src/App.jsx
+++ project-demo/src/App.jsx
@@ -2,18 +2,45 @@ import React from "react";
 import {createRoot} from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 root.render(
+<>
+  <Header/>
+  <MainContent/>
+  <Footer/>
+</>
+);
+
+
+
+
+ function Header(){
+    return(
   <>
     <img src="/src/assets/react.svg" width={100}/>
     <header><h1>Hello this is the header</h1></header>
-    <ul>
-      <li>list number one</li>
-      <li>list number two</li>
-      <li>list number three</li>
-      <li>list number four</li>
-    </ul>
+  </>
+    )
+  }
+
+function MainContent(){
+  return(
+  <>
+    
+      <ul>
+        <li>list number one</li>
+        <li>list number two</li>
+        <li>list number three</li>
+        <li>list number four</li>
+      </ul>
+      </>
+  )
+}
+
+function Footer(){
+  return (
+  <>
     <footer><span>my footer is this one </span></footer>
   </>
-   
-    
-);
+
+  )
+}
 export default App;
