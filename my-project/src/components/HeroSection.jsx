ef50e67e0fcb5769f72e3dce1a6f66d800bc1f25
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-12 20:12:03 +0000

    new

--- my-project/src/components/HeroSection.jsx
+++ my-project/src/components/HeroSection.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import Slider from "react-slick";
+const HeroData =[
+  { id: 1, src: '/src/assets/bm2.jpg', alt: 'Slide 1',
+    subtile:"subtitle of the slide 1",
+    title:"title of the slide 1",
+    description:"Description of the slide 1",
+   },
 
-const HeroSlide =[
-    {
-      id:1,
-      img: bm2
-    }
 
+  { id: 2, src: '/src/assets/bm3.jpg', alt: 'Slide 2',
+    subtile:"subtitle of the slide 2",
+    title:"title of the slide 2",
+    description:"Description of the slide 2",
+  },
+  { id: 3, src: '/src/assets/bm4.jpg', alt: 'Slide 3',
+    subtile:"subtitle of the slide 3",
+    title:"title of the slide 3",
+    description:"Description of the slide 3",
+   },
+  { id: 4, src: '/src/assets/bm5.jpg', alt: 'Slide 4' ,
+    subtile:"subtitle of the slide 4",
+    title:"title of the slide 4",
+    description:"Description of the slide 4",
+  },
+  { id: 5, src: '/src/assets/bm6.jpg', alt: 'Slide 5',
+    subtile:"subtitle of the slide 5",
+    title:"title of the slide 5",
+    description:"Description of the slide 5",
+   },
+  { id: 6, src: '/src/assets/bm7.jpg', alt: 'Slide 6', 
+    subtile:"subtitle of the slide 6",
+    title:"title of the slide 6",
+    description:"Description of the slide 6",
+  },
+  
 ]
 
 function HeroSection() {
@@ -22,33 +49,31 @@ function HeroSection() {
         pauseOnHocus: false,
 
       };
-  return (
-    <div >
-        {/* Hero Sec */}
-        <div className='w-full'>
-       <Slider {...settings}>
-       <div className='w-full'>
-        <h3>1</h3>
-      </div  >
-      <div className='w-full'>
-        <h3>2</h3>
-      </div>
-      <div className='w-full'>
-        <h3>3</h3>
-      </div>
-      <div className='w-full'>
-        <h3>4</h3>
-      </div>
-      <div className='w-full'>
-        <h3>5</h3>
-      </div>
-      <div className='w-full'>
-        <h3>6</h3>
-      </div>
-       </Slider>
+      return (
+        <div>
+          {/* Hero Sec */}
+          <div className='w-full'>
+            <Slider {...settings}>
+              {HeroData.map((data) => (
+                <div key={data.id}>
+                  <div className='grid grid-cols-1 md:grid-cols-2'>
+                    <div>
+                      <h1>{data.subtile}</h1>
+                      <h1>{data.title}</h1>
+                      <p>{data.description}</p>
+
+                      <button >
+                        Shop Now
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
         </div>
-    </div>
-  )
-}
+      );
+    }
+    
 
 export default HeroSection
