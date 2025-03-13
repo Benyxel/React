7c77c665a3c06f75796714af5a3e74da509819c5
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-13 23:11:46 +0000

    new

--- my-project/src/components/shared/Category.jsx
+++ my-project/src/components/shared/Category.jsx
@@ -23,10 +23,11 @@ const Category = () => {
 
           <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end'> 
             <div>
+              {/* Buy for me card */}
               <div className='mb-4'>
-                <p className='mb-[10px] font-bold  text-gray-400'>Need help to order from</p>
-                <p className='text-2xl font-semibold mb-[2px]'>China</p>
-                <p className='text-4xl xl:text-5xl font-bold opacity-20'>Buy for me</p>
+                <p className='mb-[0px] font-bold  text-gray-400'>Need help to order from</p>
+                <p className='text-2xl font-semibold mb-[0px]'>China</p>
+                <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-8'>Buy for me</p>
                 <Button 
                 text='Buy4me'
                 bgColor={"bg-primary"}
@@ -34,12 +35,32 @@ const Category = () => {
                 />
               </div>
             </div>
-            
+          
             <img  src={image1}
             className='w-320px absolute bottom-0 '
             />
           </div>
 
+          {/* sourcing card */}
+          <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end'> 
+            <div>
+              {/* Buy for me card */}
+              <div className='mb-4'>
+                <p className='mb-[0px] font-bold  text-gray-400'>Need help to order from</p>
+                <p className='text-2xl font-semibold mb-[0px]'>China</p>
+                <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-8'>Buy for me</p>
+                <Button 
+                text='Buy4me'
+                bgColor={"bg-primary"}
+                textColor={"text-white"}
+                />
+              </div>
+            </div>
+          
+            <img  src={image1}
+            className='w-320px absolute bottom-0 '
+            />
+          </div>
         </div>
       </div>
     </div>
