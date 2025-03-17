0585df44b5ee2d8e8a698ec126df205dea5df7aa
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-17 00:19:21 +0000

    new

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
-import {createRoot} from "react-dom/client"
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import Button from './components/shared/TrackB'
-import Category from './components/Category'
-import Category2 from './components/Category2'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+import Shop from './pages/shop'
+import Service from './pages/Service'
+import About from './pages/About'
+import Checkout from './pages/checkout'
+import Login from './pages/Login'
+import Cart from './pages/cart'
+import Buy4me from './pages/Quicklinks/Buy4me'
+import Orders from './pages/Orders'
+import Shipping from './pages/Quicklinks/Shipping'
+import Tranding from './pages/Quicklinks/Tranding'
+import Whole from './pages/Quicklinks/Whole'
+import Suppliers from './pages/Quicklinks/Suppliers'
 
  function App() {
   return (
     <div>
-      <Navbar/>
-      <HeroSection/>
-      <Category/>
-      <Category2/>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path = '/' element={<Shop/>}/>
+          <Route path = '/' element={<Service/>}/>
+          <Route path = '/' element={<About/>}/>
+          <Route path = '/' element={<Checkout/>}/>
+          <Route path = '/' element={<Login/>}/>
+          <Route path = '/' element={<Cart/>}/>
+          <Route path = '/' element={<Buy4me/>}/>
+          <Route path = '/' element={<Orders/>}/>
+          <Route path = '/' element={<Shipping/>}/>
+          <Route path = '/' element={<Tranding/>}/>
+          <Route path = '/' element={<Whole/>}/>
+          <Route path = '/' element={<Suppliers/>}/>
+      </Routes>
+    
     </div>
   )
 }
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import Category from '../components/Category'
+import Category2 from '../components/Category2'
+
 
 const home = () => {
   return (
     <div>
-      
+       <Navbar/>
+      <HeroSection/>
+      <Category/>
+      <Category2/>
     </div>
   )
 }
