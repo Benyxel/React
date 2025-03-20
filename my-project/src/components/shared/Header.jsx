e9133b55d20b4fa5d6dc2e6fd63ce2b869f0a148
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-20 23:02:35 +0000

    new

--- my-project/src/components/shared/Header.jsx
+++ my-project/src/components/shared/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = ({title, subtitle}) => {
   return (
-    <div>
-     <h1>{title}</h1>
-     <h2>{subtitle}</h2>
+    <div className='text-center mb-10w-[600px] mx-auto space-y-1 '>
+     <h1 className='text-3xl font-bold lg:text '>{title}</h1>
+     <p className=' text-sm text-gray-400'>{subtitle}</p>
     </div>
   )
 }
