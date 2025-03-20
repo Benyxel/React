b66aedc878fd380c728b43e6b0be41cd15231eb7
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-20 00:32:27 +0000

    new

--- my-project/src/components/ServicesC.jsx
+++ my-project/src/components/ServicesC.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { FaCarSide } from "react-icons/fa";
-import { FaCheckCircle } from "react-icons/fa";
-import { FaWallet } from "react-icons/fa";
-import { FaHeadphonesAlt } from "react-icons/fa";
-
+import { FaCarSide, FaCheckCircle, FaWallet, FaHeadphonesAlt } from 'react-icons/fa';
 
 const ServiceData = [
   {
@@ -34,17 +30,18 @@ const ServiceData = [
 
 const ServicesC = () => {
   return (
-    <div>
+    <div className='overflow-hidden'>
       <div className='container my-14 md:my-20'>
-        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8'>
-          {ServiceData.map((data) => (
-            <div key={data.id} className='flex flex-col items-start sm:flex-row gap-4 gap-y-4'>
-              {data.icon}
-              <div>
-              <h1 className='lg:text-xl font-bold'>{data.title}</h1>
-              <h1 className='text-gray-400 text-sm'>{data.description}</h1>
+        <div className='flex animate-scroll'>
+          {ServiceData.concat(ServiceData).map((data, index) => (
+            <div key={index} className='flex-shrink-0 w-64 p-4'>
+              <div className='flex flex-col items-start sm:flex-row gap-4 gap-y-4'>
+                {data.icon}
+                <div>
+                  <h1 className='lg:text-xl font-bold'>{data.title}</h1>
+                  <h1 className='text-gray-400 text-sm'>{data.description}</h1>
+                </div>
               </div>
-              
             </div>
           ))}
         </div>
