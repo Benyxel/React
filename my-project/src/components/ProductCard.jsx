ea119329bb26a3a8582c1311ef0628165febccf1
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-21 01:07:42 +0000

    new

--- my-project/src/components/ProductCard.jsx
+++ my-project/src/components/ProductCard.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const ProductCard = ({data}) => {
   return (
     <div>
-      
+      <div> 
+        <div>
+            <div>
+                <div>
+                    <img src={data.img} alt="" />
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
--- my-project/src/components/Products.jsx
+++ my-project/src/components/Products.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import Header from './shared/Header'
+import React from 'react';
+import Header from './shared/Header';
+import img from '../assets/products/pro1.jpg';
+import ProductCard from './ProductCard';
 
-const ProductCard =[
-    {
-      id:1,
-      img: 
-    },
-]
+const ProductData = [
+  {
+    id: 1,
+    img: img,
+    title: "Mouse",
+    price: "120",
+    aosDelay: "0"
+  },
+
+  {
+    id: 2,
+    img: img,
+    title: "Mouse",
+    price: "120",
+    aosDelay: "0"
+  },
+];
 
 const Products = () => {
   return (
     <div>
       <div className='container'>
-        <Header title= "Our Products" subtitle=" Explore Our Products"/>
-        <ProductCard data={ProductData}/>
+        <Header title="Our Products" subtitle="Explore Our Products" />
+        {ProductData.map((product) => (
+          <ProductCard key={product.id} data={product} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
