f78766ba5e9461c323078f7623e3b14e97279400
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-21 01:53:48 +0000

    new

--- my-project/src/components/ProductCard.jsx
+++ my-project/src/components/ProductCard.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const ProductCard = ({data}) => {
+const ProductCard = ({ data }) => {
   return (
-    <div>
-      <div> 
-        <div>
-            <div>
-                <div>
-                    <img src={data.img} alt="" />
-                </div>
-            </div>
-        </div>
+    <div className='mb-10 mt-[35px] flex flex-col items-center '>
+      <div>
+        <img src={data.img} alt={data.title} className='[h-230px] w-[260px] object-cover rounded-md' />
+      </div>
+      <div className='mt-4 text-center'>
+        <h2 className='text-lg font-bold'>{data.title}</h2>
+        <p className='text-gray-500'>₵{data.price}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- my-project/src/components/Products.jsx
+++ my-project/src/components/Products.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './shared/Header';
-import img from '../assets/products/pro1.jpg';
+import img from '../assets/products/pro1.png';
 import ProductCard from './ProductCard';
 
 const ProductData = [
@@ -11,9 +11,22 @@ const ProductData = [
     price: "120",
     aosDelay: "0"
   },
-
   {
     id: 2,
+    img: img,
+    title: "Mouse",
+    price: "120",
+    aosDelay: "0"
+  },
+  {
+    id: 3,
+    img: img,
+    title: "Mouse",
+    price: "120",
+    aosDelay: "0"
+  },
+  {
+    id: 4,
     img: img,
     title: "Mouse",
     price: "120",
@@ -26,9 +39,11 @@ const Products = () => {
     <div>
       <div className='container'>
         <Header title="Our Products" subtitle="Explore Our Products" />
-        {ProductData.map((product) => (
-          <ProductCard key={product.id} data={product} />
-        ))}
+        <div className='flex flex-row flex-wrap justify-center gap-10'>
+          {ProductData.map((product) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
