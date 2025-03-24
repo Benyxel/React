92eb37f7a711b6c7da11549c3bd2dc474762556f
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-24 23:59:21 +0000

    new

--- my-project/src/components/Products.jsx
+++ my-project/src/components/Products.jsx
@@ -1,38 +1,8 @@
 import React from 'react';
-import Header from './shared/Header';
-import img from '../assets/products/pro1.png';
 
 
-const ProductData = [
-  {
-    id: 1,
-    img: img,
-    title: "Mouse",
-    price: "120",
-    aosDelay: "0"
-  },
-  {
-    id: 2,
-    img: img,
-    title: "Mouse",
-    price: "120",
-    aosDelay: "0"
-  },
-  {
-    id: 3,
-    img: img,
-    title: "Mouse",
-    price: "120",
-    aosDelay: "0"
-  },
-  {
-    id: 4,
-    img: img,
-    title: "Mouse",
-    price: "120",
-    aosDelay: "0"
-  },
-];
+
+
 
 const Products = () => {
   return (
