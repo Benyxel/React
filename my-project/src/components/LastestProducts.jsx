4f6d9e53206aa48a97839acaacc5ca26de782c4b
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-24 23:53:58 +0000

    new

--- my-project/src/components/LastestProducts.jsx
+++ my-project/src/components/LastestProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
+import ProductItem from './productItem'
 
 const LastestProducts = () => {
     const { products } = useContext(ShopContext)
@@ -17,7 +18,14 @@ const LastestProducts = () => {
             <p className=' w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Explore our best porducts now check out more from the shop page </p>
            </div>
 
-           
+           {/* Rendering products */}
+           <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gaps-4 gap-y-6'>
+                {
+                    LatestGoods.map((item,index)=>(
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+                    ))
+                }
+           </div>
         </div>
     )
 }
