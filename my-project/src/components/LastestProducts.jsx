681aa47e71a6ac758d91b8606fead9655ad3ac81
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-25 00:41:16 +0000

    new

--- my-project/src/components/LastestProducts.jsx
+++ my-project/src/components/LastestProducts.jsx
@@ -12,7 +12,7 @@ const LastestProducts = () => {
   }, [products]);
 
   return (
-    <div className='my-10'>
+    <div  className='container my-10'>
       <div className='text-center py-8 text-3xl'>
         <Title text1={"LASTEST"} text2={"PRODUCTS"} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
