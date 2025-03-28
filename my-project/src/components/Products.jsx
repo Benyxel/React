6ca3830956f68e653b74036ea754881cd8846768
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-28 23:53:33 +0000

    new

--- my-project/src/components/Products.jsx
+++ my-project/src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
 
@@ -10,27 +10,30 @@ const Products = () => {
 
   const {productsId} =useParams();
   const {products} = useContext(ShopContext);
-  const [productsData,setProductsData]= useState(fales)
-  
+  const [productsData,setProductsData]= useState(false)
+  const [image,setImage] = useState('')
   const fetchProductData = async() =>{
     products.map((item)=>{
       if(item._id===productsId){
         setProductsData(item)
+        setImage(item.image[0])
         return null;
       }
-    
-  })
+    })
+  }
 
 useEffect(()=>{
   fetchProductData();
 },[productsId])
 
-  return (
-    <div>
+  return productsData ?(
+    <div className=''>
       
       
     </div>
-  );
+  ) : <div className='opacity-0'>
+
+      </div>
 };
 
 export default Products;
