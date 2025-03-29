717b64cc6ea15dca690d395f0aee7b668d54f069
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-29 20:16:17 +0000

    new

--- my-project/src/components/Product.jsx
+++ my-project/src/components/Product.jsx
@@ -9,25 +9,46 @@ const Product = () => {
  const [productData,setProductData] = useState(false)
  const [image,setImage] = useState('')
 
- const fetchProductData = async () =>{
-  products.map((item)=>{
-    if (item._id===productId)
-      setProductData(item)
-      setImage(item.image[0])
-    return null
-  })
+ const fetchProductData = async () => {
+  products.forEach((item) => {
+    if (item._id === Number(productId)) { // Convert productId to a number
+      setProductData(item);
+      setImage(item.image[0]);
+    }
+  });
+ };
 
- }
-
- useEffect(()=>{
-  fetchProductData()
- },[productId])
+ useEffect(() => {
+  fetchProductData();
+}, [productId, products])
 
   return productData? (
 
 
-    <div>
-      
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* product data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+    {/* product images */}
+    <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+      <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+        {
+          productData.image && productData.image.length > 0 ? (
+            productData.image.map((item, index) => (
+              <img
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                alt=""
+              />
+            ))
+          ) : (
+            <p>No images available</p>
+          )
+        }
+      </div>
+    </div>
+      </div>
     </div>
   ): <div className='opacity-0'></div>
 }
