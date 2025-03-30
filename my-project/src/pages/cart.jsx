399e05e9cfd11a0b5551ffc17a46e0cf2c08dd75
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-30 21:56:27 +0000

    new

--- my-project/src/pages/cart.jsx
+++ my-project/src/pages/cart.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
 
 const cart = () => {
-  return (
-    <div>
-      cart
-    </div>
-  )
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
 
-export default cart
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className='container order-t pt-14'>
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+
+      <div>
+        {cartData.length > 0 ? (
+          cartData.map((item, index) => {
+            const productData = products.find((product) => product._id === Number(item._id));
+            return (
+              <div
+                key={index}
+                className='py-4 border-t text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'
+              >
+                <div className='flex items-start gap'>
+                  <img className='w-16 sm:w-20' src={productData?.image[0]} alt="" />
+                  <div>
+                    <p className=' mt-2 mx-6 text-xs sm:text-lg font-medium'>{productData?.name || 'Product not found'}</p>
+                  
+                  <div className='flex items-center gap-5 mx-6 mt-2'>
+                    <p>{currency} {productData.price}</p>
+                    <p className='px-2  sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                    </div>
+                  </div>
+                </div>
+
+                
+              </div>
+            );
+          })
+        ) : (
+          <p>Your cart is empty.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default cart;
