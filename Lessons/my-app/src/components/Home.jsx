b247ce166884ec8e4614b3353068c05605796b9b
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-04-14 23:26:19 +0000

    new

---
+++ Lessons/my-app/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+
+const home = () => {
+    const [isGoing, setIsGoing] = useState(false)
+
+    function mindset () {
+        setIsGoing (prev => !prev)
+    };
+
+  return (
+    <div>
+      <h1>Do I feel like Going out to night?</h1>
+        <button onClick={mindset}> { isGoing ? "Yes" : "No" } </button>
+    </div>
+  )
+}
+
+export default home
