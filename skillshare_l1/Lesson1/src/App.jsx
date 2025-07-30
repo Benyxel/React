336302c400f2d1d42e392efd6a635b6c7e27c411
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-07-30 22:23:18 +0000

    new

---
+++ skillshare_l1/Lesson1/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import Card from './components/Card'
+import Todo from './components/Todo'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Todo />
+      
+    </>
+  )
+}
+
+export default App
