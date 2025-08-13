0b511d2b7c21a23eef60be7ce945a99288c15e5d
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-08-13 22:49:47 +0000

    new

---
+++ skillshare_l1/lesson2/Movie_Project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <div className='app'>
+      <nav className='navbar'>App</nav>
+      <main className='main'>same</main>
+    </div>
+  )
+}
+
+export default App
