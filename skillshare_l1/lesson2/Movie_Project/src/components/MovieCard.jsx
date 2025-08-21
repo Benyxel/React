41ca06f204ac4b6d07a49365a64b9e31bb5c8cc4
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-08-21 23:51:18 +0000

    new

--- skillshare_l1/lesson2/Movie_Project/src/components/MovieCard.jsx
+++ skillshare_l1/lesson2/Movie_Project/src/components/MovieCard.jsx
@@ -4,8 +4,8 @@ import Star from "../assets/star.png"
 
 const MovieCard = ({movie}) => {
   return (
-      <a href='' className='movie_card'>
-          <img src={movie.poster_path} className='movie_poster' alt='movieposter' />
+      <a href={`https://www.themoviedb.org/movie/${movie.id}`} target='_blank' className='movie_card'>
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className='movie_poster' alt='movieposter'/>
           
           <div className='movie_details'>
               <h3 className='movie_d_heading'>{movie.original_title}</h3>
@@ -13,7 +13,7 @@ const MovieCard = ({movie}) => {
                   <p>{movie.release_date}</p>
                   <p>{movie.vote_average}<img src={Star} className='card_emoji' alt=''/></p>
               </div>
-              <p className='movie_des'>{movie.overview}</p>
+              <p className='movie_des'>{movie.overview.slice(0,100)+"..."}</p>
         </div>
 
     </a>
