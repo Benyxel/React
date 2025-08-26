e4856bb6e95477e8b50a9161be7ac222e64d08d0
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-08-26 22:55:53 +0000

    new

--- Lessons/my-app/src/App.jsx
+++ Lessons/my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
 
@@ -9,22 +9,23 @@ import Product3 from "./assets/non-stick-cooking-set-4-pieces.jpg"
 import LoginCard from './components/LoginCard'
 
 function App() {
-  
+  const [num, setNum] = useState(0)
 
   return (
      
-      <form> 
-
-      <LoginCard/>
-
-      </form>
-      /* <Botton btnName="Login" />
-
-      <div className='cards'>
+      
+       <>
+       {/* <div className='cards'>
         <ProductsCard  title={"Product1"} price={"GHC 23"} img={Product}/>
       <ProductsCard  title={"Product2"} price={"GHS 56"} img={Product2}/>
       <ProductsCard title={"Product3"} price={"GHC 34"} img={Product3}/>
-      </div> */
+      </div>  */}
+      <p>{ num}</p>
+      <button className='Cardbtn' onClick={() => setNum(num + 1)}> Increase</button>
+      <button className='Cardbtn' onClick={()=>setNum (num -1)}> Increase</button>
+       </>
+
+      
       
 
     
